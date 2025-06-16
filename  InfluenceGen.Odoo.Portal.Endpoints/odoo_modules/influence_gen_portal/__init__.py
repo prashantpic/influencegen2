9789984e5fde06f6003ccdf0d03f76c671c6756a
prashantpic <prashantpic001@gmail.com>
2025-06-16 13:53:46 +0000

    Files added by TheSSS-AI

---  InfluenceGen.Odoo.Portal.Endpoints/odoo_modules/influence_gen_portal/__init__.py
+++  InfluenceGen.Odoo.Portal.Endpoints/odoo_modules/influence_gen_portal/__init__.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from . import controllers
 # from . import models # If any portal-specific models are defined (unlikely, models in business layer)
 # from . import wizards # If any portal-specific wizards
---  InfluenceGen.Odoo.Portal.Endpoints/odoo_modules/influence_gen_portal/__manifest__.py
+++  InfluenceGen.Odoo.Portal.Endpoints/odoo_modules/influence_gen_portal/__manifest__.py
@@ -20,8 +20,8 @@ Includes:
         'influence_gen_business_services', # Assumed name for REPO-IGBS-003
     ],
     'data': [
-        'security/ir.model.access.csv',
-        'security/influence_gen_portal_security.xml',
+        # 'security/ir.model.access.csv', # To be created if any custom portal models are needed here (unlikely, data models in business layer)
+        # 'security/influence_gen_portal_security.xml', # For portal user group access rules - mostly controller-based
         'views/portal_layout_templates.xml',
         'views/portal_dashboard_templates.xml',
         'views/portal_profile_templates.xml',
@@ -35,35 +35,39 @@ Includes:
         'views/portal_accessibility_snippets.xml',
         'views/portal_error_templates.xml',
         'views/portal_guidance_templates.xml',
-        'views/assets.xml', # Asset definitions moved here as per Odoo best practices
-        'data/ir_ui_menu_data.xml',
+        'data/ir_ui_menu_data.xml', # Portal menu items
+        # Add other data files if necessary (e.g., default values for settings specific to portal if not business layer)
     ],
     'assets': {
         'web.assets_frontend': [
             'influence_gen_portal/static/src/scss/portal_main.scss',
             'influence_gen_portal/static/src/scss/accessibility.scss',
             'influence_gen_portal/static/src/scss/responsive.scss',
+            # Component SCSS (if any specific to components)
+            # 'influence_gen_portal/static/src/scss/components/*.scss',
+
             # JS Services
             'influence_gen_portal/static/src/js/services/portal_service.js',
             'influence_gen_portal/static/src/js/services/ai_image_service.js',
+
             # JS Utilities
             'influence_gen_portal/static/src/js/utils/accessibility_utils.js',
             'influence_gen_portal/static/src/js/utils/localization_utils.js',
-            # OWL Components
+
+            # OWL Components JS (should be loaded after core Odoo assets and services)
             'influence_gen_portal/static/src/js/components/abstract_form_component.js',
             'influence_gen_portal/static/src/js/components/file_uploader_component.js',
             'influence_gen_portal/static/src/js/components/ai_image_generator_component.js',
+
             # Main portal JS file if any orchestrating logic
-            'influence_gen_portal/static/src/js/portal_main.js',
+            # 'influence_gen_portal/static/src/js/portal_main.js', # (if needed for global portal behaviors)
         ],
-        'web.assets_qweb': [
-            'influence_gen_portal/static/src/xml/components/abstract_form_component.xml',
-            'influence_gen_portal/static/src/xml/components/file_uploader_component.xml',
-            'influence_gen_portal/static/src/xml/components/ai_image_generator_component.xml',
+         'web.assets_qweb': [ # For OWL component XML templates
+            'influence_gen_portal/static/src/xml/components/*.xml',
         ],
     },
     'installable': True,
-    'application': False,
+    'application': False, # It's an addon to the main application
     'auto_install': False,
-    'license': 'OEEL-1',
+    'license': 'OEEL-1', # Or appropriate license
 }
