426b1204c396aebf52ad2e1468289b0196849847
prashantpic <prashantpic001@gmail.com>
2025-06-16 14:04:56 +0000

    Files added by TheSSS-AI

---
+++  InfluenceGen.Odoo.Admin.Endpoints/odoo_modules/influence_gen_admin/wizard/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from . import kyc_management_wizards
+from . import campaign_management_wizards
