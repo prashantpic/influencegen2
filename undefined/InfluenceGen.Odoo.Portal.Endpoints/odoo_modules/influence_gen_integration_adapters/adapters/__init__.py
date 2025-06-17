85ff01534f201433262cd46e7ec1e4fd27c438a3
prashantpic <prashantpic001@gmail.com>
2025-06-17 19:21:27 +0530

    Delete undefined directory

--- undefined/InfluenceGen.Odoo.Portal.Endpoints/odoo_modules/influence_gen_integration_adapters/adapters/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from . import n8n_ai_adapter
-from . import kyc_service_adapter_base
-from . import example_kyc_adapter # If enabled per SDS: "If enabled"
-from . import payment_gateway_adapter_base
-from . import example_payment_adapter # If enabled per SDS: "If enabled"
--- undefined/InfluenceGen.Odoo.Portal.Endpoints/odoo_modules/influence_gen_integration_adapters/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-# odoo_modules/influence_gen_integration_adapters/utils/__init__.py
-from . import api_auth
-from . import integration_error_handler
