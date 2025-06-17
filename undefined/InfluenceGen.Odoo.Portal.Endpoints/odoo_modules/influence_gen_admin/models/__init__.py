6b858a7002906723a1c81c5c63aa8b9f43ddefe5
prashantpic <prashantpic001@gmail.com>
2025-06-17 19:52:28 +0530

    Delete undefined directory

--- undefined/InfluenceGen.Odoo.Portal.Endpoints/odoo_modules/influence_gen_admin/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import res_config_settings
-from . import ai_model_config
-from . import legal_document_version
-from . import maintenance_window
-from . import legal_hold
--- undefined/InfluenceGen.Odoo.Portal.Endpoints/odoo_modules/influence_gen_integration_adapters/dtos/__init__.py
+++
@@ -1,4 +0,0 @@
-# odoo_modules/influence_gen_integration_adapters/dtos/__init__.py
-from . import n8n_dtos
-from . import kyc_dtos
-from . import payment_dtos
