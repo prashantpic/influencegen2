97064cdb68c889d5678c1e33ddd72b693ef669e7
prashantpic <prashantpic001@gmail.com>
2025-06-17 19:38:30 +0530

    Delete undefined directory

--- undefined/InfluenceGen.Odoo.Portal.Endpoints/odoo_modules/influence_gen_shared_core/exceptions/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from .custom_exceptions import (
-    InfluenceGenValidationException,
-    InfluenceGenIntegrationException,
-    InfluenceGenConfigurationError,
-    InfluenceGenSecurityException,
-    InfluenceGenProcessingError,
-    InfluenceGenBaseException, # Added as per class definitions in custom_exceptions.py, even if not explicitly listed in SDS's __init__ for exceptions.
-)
