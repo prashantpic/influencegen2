1a3f6fc98b487215fb53b5224dad9474daa52b1f
prashantpic <prashantpic001@gmail.com>
2025-06-17 19:45:33 +0530

    Delete undefined directory

--- undefined/InfluenceGen.Odoo.Portal.Endpoints/odoo_modules/influence_gen_services/wizards/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import kyc_manual_review_wizard
-# Add other wizard imports here if any are created in the future
-# e.g.:
-# from . import campaign_bulk_action_wizard
-# from . import data_export_wizard
